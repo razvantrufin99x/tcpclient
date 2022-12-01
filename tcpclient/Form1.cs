15e1a0900a581487ed7e2be6e89a59f22f1cb0e5
RAZVAN TRUFIN <razvantrufin99x@gmail.com>
2022-12-01 21:29:30 +0200

    Add project files.

---
+++ tcpclient/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.IO;
+
+namespace tcpclient
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            tbFilename.Text = openFileDialog1.FileName;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Stream fileStream = File.OpenRead(tbFilename.Text);
+            // Alocate memory space for the file
+            byte[] fileBuffer = new byte[fileStream.Length];
+            fileStream.Read(fileBuffer, 0, (int)fileStream.Length);
+            // Open a TCP/IP Connection and send the data
+            TcpClient clientSocket = new TcpClient(tbServer.Text, 8080);
+            NetworkStream networkStream = clientSocket.GetStream();
+            networkStream.Write(fileBuffer, 0, fileBuffer.GetLength(0));
+            networkStream.Close();
+        }
+        //telnet localhost 8080
+        
+    }
+}
